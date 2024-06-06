491f5eaf97392b79cf4313287c403d3536756341
Talibiq123 <talibsaeed90@gmail.com>
2024-06-06 23:40:43 +0530

    Django Models

--- chaiaurDjango/myapp/models.py
+++ chaiaurDjango/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('ML', 'MASALA'),
+        ('GR', 'GINGER'),
+        ('KW', 'KIWI'),
+        ('PL', 'PLAIN'),
+        ('EL', 'ELACHI'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chaiaurDjango\myapp')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+
